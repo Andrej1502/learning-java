2bea214117d0f7c5cf1ff0630a200027869e3580
Andrej1502 <andrejsgazins84@gmail.com>
2024-03-16 16:09:37 +0200

    FirstHomeWork/12032024/updated16032024

---
+++ src/classroom/Repeat.java
@@ -0,0 +1,30 @@
+package classroom;
+
+public class Repeat {
+    public static void main(String[] args) {
+
+        // Primitive (Non-Primitive)
+
+        //Numbers ?
+        // 2 Categories: whole numbers & decimal numbers
+
+        byte age = 40;
+        short yearOfBirth = 1984;
+        int populationOfLatvia = 1884000;
+        long earthSize = 9999999999999L;
+
+        // Decimal numbers
+        // Floating point numbers
+
+        float weigth = 70.29f;
+        double roomSize = 30.333131d;
+
+        // Character
+        char myFirstLetterName = 'N';
+
+        //Boolean (has; is)
+        boolean isSummer = false;
+        boolean myDog = true;
+
+    }
+}
